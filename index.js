5791ec568a6fa26636e7cb32c561776d1f91ee84
Muhammad Haekal <93haekal@gmail.com>
2018-04-18 16:42:25 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const mongodb = require("mongodb");
+const assert = require("assert");
+
+// Connection URL
+const MONGO_URL = "mongodb://localhost:27017";
+
+// Database Name
+const dbName = "impactodo";
+
+const sampleTodos = [
+  {
+    text: "Learn MongoDB"
+  },
+  {
+    text: "Create todo"
+  },
+  {
+    text: "Code Node.js"
+  }
+];
+
+const insertTodo = (db, documents, callback) => {
+  db.collection("todos").insert(documents, (err, result) => {
+    assert.equal(err, null);
+    console.log("Inserted 3 todos");
+    callback(result);
+  });
+};
+
+const findTodo = (db, callback) => {
+  db
+    .collection("todos")
+    .find()
+    .toArray((err, result) => {
+      assert.equal(err, null);
+      console.log("All todos");
+      callback(result);
+    });
+};
+
+const updateTodo = (db, callback) => {
+  // db.collection("todos")
+  //   .updateOne({text: "Learn MongoDB"},{ $set: { b : 1 } })
+  db
+    .collection("todos")
+    .updateOne(
+      { text: "Learn MongoDB" },
+      { $set: { text: "Learn Mongozzzzzz" } },
+      function(err, result) {
+        assert.equal(err, null);
+        console.log("Updated !!!!!!");
+        callback(result);
+      }
+    );
+};
+
+// Use connect method to connect to the server
+mongodb.MongoClient.connect(MONGO_URL, (err, client) => {
+  assert.equal(null, err);
+
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  insertTodo(db, sampleTodos, () => {
+    console.log("inserted...");
+  });
+
+  findTodo(db, result => {
+    console.log(result);
+  });
+
+  updateTodo(db, result => {
+    // console.log(result);
+  });
+
+  findTodo(db, result => {
+    console.log(result);
+    client.close();
+  });
+});
